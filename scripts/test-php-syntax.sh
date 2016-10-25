206f114b25dcd09e2738f6377157b7dafca28946
fufroma <fufroma@nnx.com>
2016-10-25 13:54:30 +0200

    Exclude /vendor, /cache and /var/cache from PHP syntax check

--- scripts/test-php-syntax.sh
+++ scripts/test-php-syntax.sh
@@ -3,7 +3,7 @@
 # Check for syntax error in PHP files
 
 echo -e "\e[1;34mChecking syntax error\e[00m"
-output=$(find . -name '*.php' -exec php --syntax-check {} \; | grep -v 'No syntax errors detected in')
+output=$(find . -name '*.php' -not -path "./vendor/*" -not -path "./var/cache/*" -exec php --syntax-check {} \; | grep -v 'No syntax errors detected in')
 if [[ $output ]]
 then 
     echo -e '\e[00;31mPlease check files syntax\e[00m'
